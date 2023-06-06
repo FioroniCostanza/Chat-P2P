4ef13c2e1f353b3a263c22f89f86c29fe95ab606
Stefano Perone <117631640+stefanoperone01@users.noreply.github.com>
2023-06-06 11:52:40 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from peer import Peer
 from random import randint
+import signal
+
+def handler(signum, frame): # Ctrl-c handler
+    peer.stop()
+    print("Press enter to exit definitely...")
 
 if __name__ == "__main__":
-    Peer(input("Username: "), "localhost", randint(1000,9999))
+    signal.signal(signal.SIGINT, handler)
+    peer = Peer(input("Username: "), "localhost", randint(1000, 9999))
